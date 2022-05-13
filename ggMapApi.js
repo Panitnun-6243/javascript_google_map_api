712b533bf041eef909769cf0543562635180574c
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-05-14 03:06:34 +0700

    Update api key and Add map marker

--- ggMapApi.js
+++ ggMapApi.js
@@ -1,5 +1,13 @@
 function myMap(){
+    var mapLatLng = {lat: 17.9004781, lng: 99.604355}
     var mapCanvas = document.getElementById("map")
-    var mapOptions = {center: new google.maps.LatLng(18.6916598,97.6683007), zoom: 8, mapTypeId: 'roadmap'} 
+    var mapOptions = {center: mapLatLng, zoom: 8, mapTypeId: 'roadmap'} 
     var map = new google.maps.Map(mapCanvas, mapOptions)
+    var marker = new google.maps.Marker({
+        positon: mapLatLng,
+        title: "I'm here",
+    })
+    marker.setMap(map)
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+        
 }
