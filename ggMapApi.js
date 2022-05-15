0e9f195bfc6fcf5f0ec7dc63f5bf04a5a407bc5f
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-05-15 09:06:43 +0700

    Add listener form marker to get latlng

--- ggMapApi.js
+++ ggMapApi.js
@@ -6,11 +6,13 @@ function myMap() {
   var marker = new google.maps.Marker({
     position: mapLatLng,
     title: "I'm lived here",
+    draggable: true,
   }); //map marker
 
   var marker2 = new google.maps.Marker({
     position: new google.maps.LatLng(13.6513, 100.4964),
     title: "My univerity",
+    draggable: true,
   }); //map marker2
 
   marker.setMap(map); //set map for marker
@@ -18,21 +20,49 @@ function myMap() {
   marker2.setMap(map); //set map for marker2
   marker2.setAnimation(google.maps.Animation.DROP); //marker2 animation
 
-  setTimeout(
-    () => {
-      marker.setMap(null);
-      marker = null;
-    },
+  // setTimeout(
+  //   () => {
+  //     marker.setMap(null);
+  //     marker = null;
+  //   },
 
-    15000
-  ); //set marker timeout
+  //   15000
+  // ); //set marker timeout
 
-  setTimeout(
-    () => {
-      marker2.setMap(null);
-      marker2 = null;
-    },
+  // setTimeout(
+  //   () => {
+  //     marker2.setMap(null);
+  //     marker2 = null;
+  //   },
 
-    15000
-  ); //set marker2 timeout
+  //   15000
+  // ); //set marker2 timeout
+
+  google.maps.event.addListener(marker, "dragend", function (evt) {
+    document.getElementById("marker1").innerHTML =
+      "<div>Marker1 dropped: Current Lat: " +
+      evt.latLng.lat().toFixed(3) +
+      " Current Lng: " +
+      evt.latLng.lng().toFixed(3) +
+      "</div>";
+  });
+
+  google.maps.event.addListener(marker, "dragstart", function (evt) {
+    document.getElementById("marker1").innerHTML =
+      "<div>Currently dragging marker...</div>";
+  });//get Lat Lng from marker1
+
+  google.maps.event.addListener(marker2, "dragend", function (evt) {
+    document.getElementById("marker2").innerHTML =
+      "<div>Marker2 dropped: Current Lat: " +
+      evt.latLng.lat().toFixed(3) +
+      " Current Lng: " +
+      evt.latLng.lng().toFixed(3) +
+      "</div>";
+  });
+
+  google.maps.event.addListener(marker2, "dragstart", function (evt) {
+    document.getElementById("marker2").innerHTML =
+      "<div>Currently dragging marker...</div>";
+  });//get Lat Lng from marker2
 }
